997f27f0034bae759ead78237e49a48a6de0507b
TarikC03 <45046515+TarikC03@users.noreply.github.com>
2019-12-11 13:24:12 -0800

    Add files via upload

---
+++ chick-fil-a/script.js
@@ -0,0 +1,100 @@
+var a = document.getElementById("sandwhich")
+var b = document.getElementById("nuggets")
+var c = document.getElementById("deluxe")
+
+function changeImage() {
+	document.getElementById("sandwhich").src = "images/chick_which.png";
+	// document.getElementById("sandwhich") --> find sandwhich
+	// src = "nuggets.png" --> Change the source 
+	
+	
+	
+	
+	
+	
+}
+
+function changeImage1() {
+
+document.getElementById("nuggets").src = "images/nuggets.png";
+// document.getElementById("sandwhich") --> find sandwhich
+	// src = "nuggets.png" --> Change the source 
+
+
+
+}
+
+
+
+function changeImage8() {
+
+document.getElementById("deluxe").src = "images/deluxe.png";
+
+// document.getElementById("sandwhich") --> find sandwhich
+	// src = "nuggets.png" --> Change the source 
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+/*
+function changeImage1() {
+	document.getElementById("nuggets").src = "chick_which.png";
+	// document.getElementById("Chicken") --> find Chicken
+	// src = "chick_which.png" --> Change the source 
+	
+	if (b.src.match("chick-fil-a/zestyBuffalo.png"))
+		b.src = "chick-fil-a/nuggets.png";{
+	}else if (b.src.match("chick-fil-a/nuggets.png")){
+		b.src = "chick-fil-a/nuggets.png";
+     }else{
+	 } 
+}
+	
+
+
+function changeImage2() {
+	document.getElementById("deluxe").src = "chick_which.png";
+	// document.getElementById("Chicken") --> find Chicken
+	// src = "chick_which.png" --> Change the source 
+	
+
+	if (c.src.match("chick-fil-a/Sauce_Garden-Herb-Ranch_Desk.png"))
+		c.src = "chick-fil-a/deluxe.png";{
+	}else if (c.src.match("chick-fil-a/deluxe.png")){
+		c.src = "chick-fil-a/deluxe.png";
+     }else{
+	 }
+}
+*/
+/*
+
+if (a.src.match("chick-fil-a/sauce.png"))
+		a.src = "chick-fil-a/chick_which.png";{
+	}else if (a.src.match("chick-fil-a/chick_which.png")){
+		a.src = "chick-fil-a/chick_which.png";
+     }else{
+	 }
+}
+
+
+
+*/
+
+
+
+
+
+
+//if(a.src.match("zestyBuffalo.png")){
+//		a.src = "sandwhich.png";
+//	} 
